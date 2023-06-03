dbe719df17280096901902307ae51ecbac31e63d
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-06-03 22:56:08 +0900

    [#7] feat: 프로젝트 기본 설정
    feat: 프로젝트 기본 설정 작성

---
+++ src/test/java/com/Chumaengi/chumaengi/ChumaengiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Chumaengi.chumaengi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChumaengiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
