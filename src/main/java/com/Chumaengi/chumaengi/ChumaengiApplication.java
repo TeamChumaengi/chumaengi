3bd0ec48c65098fefeeab1a83861e45e527c5636
SunKyoung Yoon <yskkjdh@gmail.com>
2023-06-03 22:33:22 +0900

    feat: 프로젝트 기본 설정

---
+++ src/main/java/com/Chumaengi/chumaengi/ChumaengiApplication.java
@@ -0,0 +1,13 @@
+package com.Chumaengi.chumaengi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChumaengiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChumaengiApplication.class, args);
+	}
+
+}
