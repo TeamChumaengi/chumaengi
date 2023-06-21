929f3419d17faae520b6b2cc4b9960e9395948f8
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-06-21 23:46:19 +0900

    [#15] feat: Member 엔티티를 설계한다
    * feat: 프로젝트 기본 설정
    
    * feat: Member 엔티티 및 BaseTimeEntity 구현

---
+++ src/test/java/com/Chumaengi/chumaengi/fixture/MemberFixture.java
@@ -0,0 +1,23 @@
+package com.Chumaengi.chumaengi.fixture;
+
+import com.Chumaengi.chumaengi.member.domain.Member;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberFixture {
+    SUNKYOUNG("윤선경", "qwe1234@gmail.com", null, "qwe1234", "추맹이1"),
+    HOEUN("이호은", "qwe5678@gmail.com", null, "qwe5678", "추맹이2"),
+    YUJIN("소유진", "asd1234@gmail.com", null, "asd1234", "추맹이3"),
+    HEESUN("박희선", "asd5678@gmail.com", null, "asd5678", "추맹이4")
+    ;
+
+    private final String name;
+    private final String email;
+    private final String picture;
+    private final String password;
+    private final String nickname;
+
+    public Member toMember() { return Member.createMember(name, email, picture, password, nickname); }
+}
