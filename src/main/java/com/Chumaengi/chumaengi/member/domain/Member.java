b2cfe99fd1382163ecc36ded94f0c0a562b81db9
SunKyoung Yoon <yskkjdh@gmail.com>
2023-06-22 21:50:56 +0900

    feat: Authority 클래스 추가

--- src/main/java/com/Chumaengi/chumaengi/member/domain/Member.java
+++ src/main/java/com/Chumaengi/chumaengi/member/domain/Member.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -23,27 +25,30 @@ public class Member extends BaseTimeEntity {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "picture")
-    private String picture;
-
     @Column(name = "password", nullable = false)
     private String password;
 
     @Column(name = "nickname", nullable = false, unique = true)
     private String nickname;
 
+    @OneToMany(mappedBy = "member", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<Authority> roles = new ArrayList<>();
+
     @Builder
-    public Member(String name, String email, String picture,
-                   String password, String nickname) {
+    public Member(String name, String email, String password, String nickname) {
         this.name = name;
         this.email = email;
-        this.picture = picture;
         this.password = password;
         this.nickname = nickname;
     }
 
-    public static Member createMember(String name, String email, String picture,
-                                      String password, String nickname){
-        return new Member(name, email, picture, password, nickname);
+    public static Member createMember(String name, String email, String password, String nickname){
+        return new Member(name, email, password, nickname);
+    }
+
+    public void setRoles(List<Authority> role) {
+        this.roles = role;
+        role.forEach(o -> o.setMember(this));
     }
 }
