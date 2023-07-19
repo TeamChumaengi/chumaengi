afc559ab347bdc51cc1b0ded2feb40ee9477a7d9
SunKyoung Yoon <yskkjdh@gmail.com>
2023-07-20 00:06:17 +0900

    refactor: BOARD_001 오타 수정 회원 -> 게시글

--- src/main/java/com/Chumaengi/chumaengi/board/exception/BoardErrorCode.java
+++ src/main/java/com/Chumaengi/chumaengi/board/exception/BoardErrorCode.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum BoardErrorCode implements ErrorCode {
-    BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "BOARD_001", "회원 정보를 찾을 수 없습니다."),
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "BOARD_002", "카테고리 정보를 찾을 수 없습니다.")
+    BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "BOARD_001", "게시글 정보를 찾을 수 없습니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "BOARD_002", "카테고리 정보를 찾을 수 없습니다."),
+    USER_IS_NOT_BOARD_WRITER(HttpStatus.CONFLICT, "BOARD_003", "게시글 작성자가 아닙니다.")
     ;
 
     private final HttpStatus status;
