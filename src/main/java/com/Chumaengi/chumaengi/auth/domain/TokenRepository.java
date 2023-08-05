4bebc53207b071f00cca61dad979562d90f15de2
SunKyoung Yoon <yskkjdh@gmail.com>
2023-08-05 18:07:35 +0900

    refactor: 사용하지 않는 클래스 제거

--- src/main/java/com/Chumaengi/chumaengi/auth/domain/TokenRepository.java
+++
@@ -1,6 +0,0 @@
-package com.Chumaengi.chumaengi.auth.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TokenRepository extends CrudRepository<Token, Long> {
-}
