f5b599c33fea2d25b1c11ded921aefd80ba8cffd
SunKyoung Yoon <yskkjdh@gmail.com>
2023-08-08 01:45:46 +0900

    feat: 카테고리별 게시글 수정 UI 구현 및 연결

--- src/main/resources/static/js/boardnotice.js
+++ src/main/resources/static/js/boardnotice.js
@@ -4,8 +4,15 @@ var main = {
         $('#boardNotice_list').on('click', function () {
             location.href="/api/boards/list/3";
         });
+        $('#boardNotice_detail').on('click', function () {
+            var boardId = $('#inputBid').val();
+            location.href="/api/boards/detail/"+boardId;
+        });
         $('#boardNotice_save').on('click', function () {
             _this.boardNotice_save();
+        });
+        $('#boardNotice_update').on('click', function () {
+            _this.boardNotice_update();
         });
     },
 
@@ -40,6 +47,46 @@ var main = {
                     location.href = '/api/boards/list/3';
                 }else{
                     alert("게시글 등록에 실패했습니다");
+                    return false;
+                }
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            })
+        }
+    },
+
+    boardNotice_update: function (){
+        var boardId = $('#inputBid').val();
+        var memberId = $('#inputId').val();
+
+        var data = {
+            category: $('#inputCategory').val(),
+            title: $('#inputTitle').val(),
+            content: $('#inputContent').val()
+        };
+        if(!data.category||data.category.trim()==""){
+            alert("카테고리를 입력해주세요");
+            return false;
+        }
+        else if(!data.title||data.title.trim()==""){
+            alert("제목을 입력해주세요");
+            return false;
+        }
+        else if(!data.content||data.content.trim()==""){
+            alert("내용을 입력해주세요");
+            return false;
+        }
+        else{
+            $.ajax({
+                type: 'PATCH',
+                url: '/api/boards/'+memberId+'/'+boardId,
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data),
+            }).done(function (data) {
+                if(data == true){
+                    location.href = '/api/boards/detail/'+boardId;
+                }else{
+                    alert("게시글 수정에 실패했습니다");
                     return false;
                 }
             }).fail(function (error) {
--- src/main/resources/static/js/question.js
+++ src/main/resources/static/js/question.js
@@ -4,8 +4,15 @@ var main = {
         $('#question_list').on('click', function () {
             location.href="/api/boards/list/1";
         });
+        $('#question_detail').on('click', function () {
+            var boardId = $('#inputQid').val();
+            location.href="/api/boards/detail/"+boardId;
+        });
         $('#question_save').on('click', function () {
             _this.question_save();
+        });
+        $('#question_update').on('click', function () {
+            _this.question_update();
         });
     },
 
@@ -40,6 +47,46 @@ var main = {
                     location.href = '/api/boards/list/1';
                 }else{
                     alert("게시글 등록에 실패했습니다");
+                    return false;
+                }
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            })
+        }
+    },
+
+    question_update: function (){
+        var boardId = $('#inputQid').val();
+        var memberId = $('#inputId').val();
+
+        var data = {
+            category: $('#inputCategory').val(),
+            title: $('#inputTitle').val(),
+            content: $('#inputContent').val()
+        };
+        if(!data.category||data.category.trim()==""){
+            alert("카테고리를 입력해주세요");
+            return false;
+        }
+        else if(!data.title||data.title.trim()==""){
+            alert("제목을 입력해주세요");
+            return false;
+        }
+        else if(!data.content||data.content.trim()==""){
+            alert("내용을 입력해주세요");
+            return false;
+        }
+        else{
+            $.ajax({
+                type: 'PATCH',
+                url: '/api/boards/'+memberId+'/'+boardId,
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data),
+            }).done(function (data) {
+                if(data == true){
+                    location.href = '/api/boards/detail/'+boardId;
+                }else{
+                    alert("게시글 수정에 실패했습니다");
                     return false;
                 }
             }).fail(function (error) {
